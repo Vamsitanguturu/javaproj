3d00da90f2becb03e18a7c6e6f41bfa3d996ed8c
unknown <vamsitanguturu@120@gmail.com>
2024-04-14 23:48:41 +0530

    created project

---
+++ src/javaproj/Main.java
@@ -0,0 +1,11 @@
+package javaproj;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Helloworld" + "Manasa");
+		// TODO Auto-generated method stub
+
+	}
+
+}
